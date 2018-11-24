49a243be01f3497720e6e570dfdd84fd0400ce2e
chenxuan <chenxuan@yusys.com.cn>
2018-11-24 22:06:02 +0800

    初始化

---
+++ src/main/java/com/chenxuanplus/sleeperframework/SleeperframeworkApplication.java
@@ -0,0 +1,12 @@
+package com.chenxuanplus.sleeperframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleeperframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleeperframeworkApplication.class, args);
+	}
+}
