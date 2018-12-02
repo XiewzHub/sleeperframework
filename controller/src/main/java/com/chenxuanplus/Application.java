98fc9f77d511a9ef325c6eb9e61985c93effbc5f
chenxuan <chenxuan@yusys.com.cn>
2018-12-03 01:15:20 +0800

    修改controller启动方式为tomcat

--- controller/src/main/java/com/chenxuanplus/Application.java
+++ controller/src/main/java/com/chenxuanplus/Application.java
@@ -3,12 +3,19 @@ package com.chenxuanplus;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.chenxuanplus.mapper")
-public class Application {
+public class Application  extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	//public static mainvoid main(String[] args) {
+		//SpringApplication.run(Application.class, args);
+	//}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(Application.class);
 	}
 }
